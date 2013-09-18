86872a439f6f80a23647e52f75f88a80da9e97aa
Matt <morris.matts@gmail.com>
2013-09-18 15:04:40 -0400

    Adding South for DB migrations

--- paws/settings.py
+++ paws/settings.py
@@ -137,6 +137,8 @@ INSTALLED_APPS = (
     'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     'django.contrib.admindocs',
+    'south',
+    'timeslots',
 )
 
 # A sample logging configuration. The only tangible logging
