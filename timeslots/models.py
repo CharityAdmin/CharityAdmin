a53e057796ffa8e45fa50668e382068cd6138d0d
Jason Bennett <j2thebennett@gmail.com>
2013-10-06 18:14:18 -0400

    Initial checkin of timeslot models, and new git ignore

--- timeslots/models.py
+++ timeslots/models.py
@@ -1,3 +1,56 @@
 from django.db import models
 
-# Create your models here.
+
+class ClientOpening(models.Model):
+    def __unicode__(self):
+        return "Client %d, Type %s, Start: %s" % (self.clientId, self.type, self.startDate)
+
+    clientId = models.IntegerField()
+    startDate = models.DateTimeField('Start Date')
+    endDate = models.DateTimeField('End Date')
+    type = models.CharField(max_length=20)
+    notes = models.CharField(max_length=255)
+
+
+class ClientOpeningMetadata(models.Model):
+    def __unicode__(self):
+        return "Client opening %s, %s " % (self.clientOpening, self.metadata)
+
+    clientOpening = models.ForeignKey(ClientOpening, db_column='clientOpeningId')
+    metadata = models.CharField(max_length=20)
+
+
+class ClientOpeningException(models.Model):
+    def __unicode__(self):
+        return "Client opening %s, %s " % (self.clientOpening, self.date)
+
+    clientOpening = models.ForeignKey(ClientOpening, db_column='clientOpeningId')
+    date = models.DateTimeField('Exception Date')
+
+
+class VolunteerCommitment(models.Model):
+    def __unicode__(self):
+        return "Volunteer %d, Type %s, Start: %s" % (self.volunteerId, self.type, self.startDate)
+
+    clientOpening = models.ForeignKey(ClientOpening, db_column='clientOpeningId')
+    volunteerId = models.IntegerField()
+    startDate = models.DateTimeField('Start Date')
+    endDate = models.DateTimeField('End Date')
+    type = models.CharField(max_length=20)
+    notes = models.CharField(max_length=255)
+
+
+class VolunteerCommitmentMetadata(models.Model):
+    def __unicode__(self):
+        return "Client opening %s, %s " % (self.clientOpening, self.metadata)
+
+    volunteerCommitment = models.ForeignKey(VolunteerCommitment, db_column='volunteerCommitmentId')
+    metadata = models.CharField(max_length=20)
+
+
+class ClientCommitmentException(models.Model):
+    def __unicode__(self):
+        return "Client opening %s, %s " % (self.clientOpening, self.date)
+
+    volunteerCommitment = models.ForeignKey(VolunteerCommitment, db_column='volunteerCommitmentId')
+    date = models.DateTimeField('Exception Date')
