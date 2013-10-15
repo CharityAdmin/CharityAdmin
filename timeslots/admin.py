0da12419e76110d76df88762ca367d75ae98107b
Matt <morris.matts@gmail.com>
2013-10-15 11:45:15 -0400

    Adding models for clients and volunteers, plus setting up the admin for these

---
+++ timeslots/admin.py
@@ -0,0 +1,4 @@
+from django.contrib import admin
+from timeslots.models import Volunteer, Client
+admin.site.register(Volunteer)
+admin.site.register(Client)
--- timeslots/models.py
+++ timeslots/models.py
@@ -1,37 +1,70 @@
 from django.db import models
+from django.contrib.auth.models import User
 
+# class Profile(models.Model):
+#     TYPE_CHOICES = (
+#         ('Admin', 'Admin'),
+#         ('Volunteer', 'Volunteer'),
+#         ('Client', 'Client'),
+#     )
+
+#     user = models.OneToOneField(User)
+#     type = models.CharField(max_length=9, choices=TYPE_CHOICES, default='Volunteer')
+
+# class Volunteer(Profile):
+#     trained = models.Boolean(default=False)
+#     clients = models.ManyToManyField('Client', related_name='volunteers')
+
+#     def __unicode__(self):
+#         return (self.user.first_name || self.user.last_name) ? self.user.first_name + " " + self.user.last_name : self.user.email
+
+# class Client(Profile):
+
+#     def __unicode__(self):
+#         return (self.user.first_name || self.user.last_name) ? self.user.first_name + " " + self.user.last_name : self.user.email
+
+class Volunteer(models.Model):
+    user = models.OneToOneField(User)
+    trained = models.BooleanField(default=False)
+    clients = models.ManyToManyField('Client', related_name='volunteers')
+
+    def __unicode__(self):
+        return self.user.first_name + " " + self.user.last_name if (self.user.first_name or self.user.last_name) else self.user.email
+
+class Client(models.Model):
+    user = models.OneToOneField(User)
+
+    def __unicode__(self):
+        return self.user.first_name + " " + self.user.last_name if (self.user.first_name or self.user.last_name) else self.user.email
 
 class ClientOpening(models.Model):
-    def __unicode__(self):
-        return "Client %d, Type %s, Start: %s" % (self.clientId, self.type, self.startDate)
-
     clientId = models.IntegerField()
     startDate = models.DateTimeField('Start Date')
     endDate = models.DateTimeField('End Date')
     type = models.CharField(max_length=20)
     notes = models.CharField(max_length=255)
 
+    def __unicode__(self):
+        return "Client %d, Type %s, Start: %s" % (self.clientId, self.type, self.startDate)
+
 
 class ClientOpeningMetadata(models.Model):
-    def __unicode__(self):
-        return "Client opening %s, %s " % (self.clientOpening, self.metadata)
-
     clientOpening = models.ForeignKey(ClientOpening, db_column='clientOpeningId')
     metadata = models.CharField(max_length=20)
 
+    def __unicode__(self):
+        return "Client opening %s, %s " % (self.clientOpening, self.metadata)
+
 
 class ClientOpeningException(models.Model):
-    def __unicode__(self):
-        return "Client opening %s, %s " % (self.clientOpening, self.date)
-
     clientOpening = models.ForeignKey(ClientOpening, db_column='clientOpeningId')
     date = models.DateTimeField('Exception Date')
 
+    def __unicode__(self):
+        return "Client opening %s, %s " % (self.clientOpening, self.date)
+
 
 class VolunteerCommitment(models.Model):
-    def __unicode__(self):
-        return "Volunteer %d, Type %s, Start: %s" % (self.volunteerId, self.type, self.startDate)
-
     clientOpening = models.ForeignKey(ClientOpening, db_column='clientOpeningId')
     volunteerId = models.IntegerField()
     startDate = models.DateTimeField('Start Date')
@@ -39,18 +72,25 @@ class VolunteerCommitment(models.Model):
     type = models.CharField(max_length=20)
     notes = models.CharField(max_length=255)
 
+    def __unicode__(self):
+        return "Volunteer %d, Type %s, Start: %s" % (self.volunteerId, self.type, self.startDate)
+
 
 class VolunteerCommitmentMetadata(models.Model):
-    def __unicode__(self):
-        return "Client opening %s, %s " % (self.clientOpening, self.metadata)
-
     volunteerCommitment = models.ForeignKey(VolunteerCommitment, db_column='volunteerCommitmentId')
     metadata = models.CharField(max_length=20)
 
+    def __unicode__(self):
+        return "Client opening %s, %s " % (self.clientOpening, self.metadata)
+
 
 class ClientCommitmentException(models.Model):
+    volunteerCommitment = models.ForeignKey(VolunteerCommitment, db_column='volunteerCommitmentId')
+    date = models.DateTimeField('Exception Date')
+
     def __unicode__(self):
         return "Client opening %s, %s " % (self.clientOpening, self.date)
 
-    volunteerCommitment = models.ForeignKey(VolunteerCommitment, db_column='volunteerCommitmentId')
-    date = models.DateTimeField('Exception Date')
+
+
+
