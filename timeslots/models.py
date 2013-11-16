6159a78f6a87a0a0d0b46468e6823bef01ef5910
Matt <morris.matts@gmail.com>
2013-11-16 01:39:33 -0500

    Updating models with first crack at getting upcoming open dates for a particular client

--- timeslots/models.py
+++ timeslots/models.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+import datetime
 from django.db import models
 from django.contrib.auth.models import User
 
@@ -32,16 +32,73 @@ class Client(models.Model):
 
 
 class ClientOpening(models.Model):
-    client = models.ForeignKey(Client, db_column='clientId')
-    startDate = models.DateTimeField('Start Date', default=datetime.now)
+    client = models.ForeignKey(Client, db_column='clientId', related_name='openings')
+    startDate = models.DateTimeField('Start Date', default=datetime.datetime.now)
     endDate = models.DateTimeField('End Date', blank=True, null=True)
     type = models.CharField(max_length=20, choices=SCHEDULE_PATTERN_TYPE_CHOICES, default='Days of Week')
     notes = models.CharField(max_length=255, blank=True)
 
     def __unicode__(self):
-        return "%s, %s: %s (%s-%s)" % (
-        self.client, self.type, self.clientopeningmetadata_set.all()[0].metadata, dateformat.format(d=self.startDate),
-        dateformat.format(d=self.endDate) if self.endDate is not None else "")
+        return "%s, %s: %s (%s-%s)" % (self.client, self.type, self.get_all_metadata_string(), dateformat.format(d=self.startDate), dateformat.format(d=self.endDate) if self.endDate is not None else "")
+
+    def get_list_title(self):
+        return "%s: %s (%s-%s)" % (self.type, self.get_all_metadata_string(), dateformat.format(d=self.startDate), dateformat.format(d=self.endDate) if self.endDate is not None else "")
+
+    def get_next_unfilled(self):
+        # get list of openings that haven't been filled
+        open_metadata_set = self.get_unfilled_metadata_set()
+
+        # TODO: make this respect commitment end dates
+        # TODO: check for exceptions too
+
+        next_opening = None
+
+        if len(open_metadata_set):
+            # if we have any open metadata, figure out the next day as a datetime object
+            if self.type in ["Days of Week", "Days of Alternating Week"]:
+                # get the first element in the set (by day-of-the-week order)
+                # TODO: make alternating weeks work
+                list_of_days = ['M', 'T', 'W', 'TH', 'F', 'SA', 'SU']
+                for idx, day in enumerate(list_of_days):
+                    if day in open_metadata_set:
+                        now = datetime.datetime.now()
+                        next_opening = datetime.datetime.now()
+                        # adjust the time to match the startDate time
+                        next_opening = next_opening.replace(hour=self.startDate.hour, minute=self.startDate.minute, second=self.startDate.second)
+                        # adjust the datetime based on the day of the week
+                        next_opening = next_opening + datetime.timedelta(days=idx-now.weekday())
+                        if now.weekday() >= idx:
+                            # if we've passed the correct day, we want the same day next week
+                            next_opening = next_opening + datetime.timedelta(weeks=1)
+                        break
+            elif self.type == "Day of Month":
+                # this assumes that Day-of-month type can only hold a single metadata each
+                now = datetime.now()
+                next_opening = now.replace(day=open_metadata_set[0], hour=self.startDate.hour, minute=self.startDate.minute, second=self.startDate.second)
+                if now.day > open_metadata_set[0]:
+                    # if we've already passed the correct day of the month, look at next month instead
+                    next_opening = next_opening + datetime.timedelta(month=1)
+            else:
+                # this is a one-off type
+                next_opening = self.startDate
+        return next_opening
+
+    def is_filled(self):
+        return self.next_opening
+
+    def get_all_metadata_set(self):
+        return set([ metadataobj.metadata for metadataobj in self.clientopeningmetadata_set.all() ])
+
+    def get_unfilled_metadata_set(self):
+        return set([ metadataobj.metadata for metadataobj in self.clientopeningmetadata_set.all() if not metadataobj.is_filled() ])
+
+    def get_all_metadata_string(self):
+        if self.type in ["Days of Week", "Days of Alternating Week"]:
+            join_string = ''
+        else:
+            join_string = ', '
+        combinedmetadata = join_string.join(self.get_all_metadata_set())
+        return combinedmetadata
 
 
 class ClientOpeningMetadata(models.Model):
@@ -50,6 +107,9 @@ class ClientOpeningMetadata(models.Model):
 
     def __unicode__(self):
         return "%s, %s: %s" % (self.clientOpening.client, self.clientOpening.type, self.metadata)
+
+    def is_filled(self):
+        return self.metadata in set([metadataobj.metadata for commitment in self.clientOpening.volunteercommitment_set.all() for metadataobj in commitment.volunteercommitmentmetadata_set.all()])
 
 
 class ClientOpeningException(models.Model):
@@ -62,8 +122,8 @@ class ClientOpeningException(models.Model):
 
 class VolunteerCommitment(models.Model):
     clientOpening = models.ForeignKey(ClientOpening, db_column='clientOpeningId')
-    volunteer = models.ForeignKey(Volunteer, db_column='volunteerId')
-    startDate = models.DateTimeField('Start Date', default=datetime.now)
+    volunteer = models.ForeignKey(Volunteer, db_column='volunteerId', related_name="commitments")
+    startDate = models.DateTimeField('Start Date', default=datetime.datetime.now)
     endDate = models.DateTimeField('End Date', blank=True, null=True)
     type = models.CharField(max_length=20, choices=SCHEDULE_PATTERN_TYPE_CHOICES, default='Days of Week')
     notes = models.CharField(max_length=255, blank=True)
